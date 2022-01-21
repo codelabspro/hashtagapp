58e28b6aa03136ff623e6dbd4c15dc4bebab1607
Arunabh Das <arunabhdas@gmail.com>
2022-01-21 18:12:06 -0500

    Added TodoRepositoryImpl

---
+++ hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/data/TodoRepositoryImpl.kt
@@ -0,0 +1,27 @@
+package com.codesdk.mobile.hashtagapp.data
+
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Created by Das on 2022-01-21.
+ */
+class TodoRepositoryImpl(
+    private val dao: TodoDao
+): TodoRepository {
+    override suspend fun insertTodo(todo: Todo) {
+        dao.insertTodo(todo)
+    }
+
+    override suspend fun deleteTodo(todo: Todo) {
+        dao.deleteTodo(todo)
+    }
+
+    override suspend fun getTodoById(id: Int): Todo? {
+        return dao.getTodoById(id)
+    }
+
+    override fun getTodos(): Flow<List<Todo>> {
+        return dao.getTodos()
+    }
+
+}
