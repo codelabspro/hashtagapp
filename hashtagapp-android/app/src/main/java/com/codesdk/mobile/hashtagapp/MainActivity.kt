bbced4d7c465dc21d1b62298176bfa8e271f3e4b
Arunabh Das <arunabhdas@gmail.com>
2022-01-21 15:07:04 -0500

    Getting Main running

--- hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/MainActivity.kt
+++ hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.codesdk.mobile.hashtagapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+    fun main() {
+
+        val channelName: String = "CodeSDK"
+        var weight = 160
+        var percentage: Double = 2.5
+        var isAvailable: Boolean = true
+        var s1 = "Call me"
+        var s2 = " Maybe"
+        val combined = s1 + s2
+        println(combined)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        main()
     }
 }
---
+++ hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.codesdk.mobile.hashtagapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Created by Das on 2022-01-19.
+ */
+class Color {
+    val Purple200 = Color(0xFFBB86FC)
+    val Purple500 = Color(0xFF6200EE)
+    val Purple700 = Color(0xFF3700B3)
+    val Teal200 = Color(0xFF03DAC5)
+}
