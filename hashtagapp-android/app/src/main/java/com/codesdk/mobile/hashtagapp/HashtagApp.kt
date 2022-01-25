7cfd16d75a7b16ce83d182414acc98a591ee3171
Arunabh Das <arunabhdas@gmail.com>
2022-01-25 12:52:08 -0500

    Added HashtagApp inherits from Application class

---
+++ hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/HashtagApp.kt
@@ -0,0 +1,10 @@
+package com.codesdk.mobile.hashtagapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Das on 2022-01-25.
+ */
+@HiltAndroidApp
+class HashtagApp: Application()
