028b7651bd14fab91766b1d672064e4d6a3c743a
Arunabh Das <arunabhdas@gmail.com>
2022-01-26 03:08:40 -0500

    Implemented handler for ADD_EDIT_TODO

--- hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/ui/todolist/TodoListViewModel.kt
+++ hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/ui/todolist/TodoListViewModel.kt
@@ -1,11 +1,14 @@
 package com.codesdk.mobile.hashtagapp.ui.todolist
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.codesdk.mobile.hashtagapp.data.TodoRepository
+import com.codesdk.mobile.hashtagapp.util.Routes
 import com.codesdk.mobile.hashtagapp.util.UiEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -26,7 +29,7 @@ class TodoListViewModel @Inject constructor(
             }
 
             is TodoListEvent.OnAddTodoClick -> {
-
+                sendUiEvent(UiEvent.Navigate(Routes.ADD_EDIT_TODO))
             }
 
             is TodoListEvent.OnUndoDeleteClick -> {
@@ -40,6 +43,12 @@ class TodoListViewModel @Inject constructor(
             is TodoListEvent.OnDoneChange -> {
 
             }
+        }
+    }
+
+    private fun sendUiEvent(event: UiEvent) {
+        viewModelScope.launch {
+            _uiEvent.send(event)
         }
     }
 
