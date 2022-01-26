8d09b74b2fa80f4785697fa837d1bc6e799a3d91
Arunabh Das <arunabhdas@gmail.com>
2022-01-26 17:04:23 -0500

    Created TodoItem

--- hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/ui/todolist/TodoListViewModel.kt
+++ hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/ui/todolist/TodoListViewModel.kt
@@ -2,6 +2,7 @@ package com.codesdk.mobile.hashtagapp.ui.todolist
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.codesdk.mobile.hashtagapp.data.Todo
 import com.codesdk.mobile.hashtagapp.data.TodoRepository
 import com.codesdk.mobile.hashtagapp.util.Routes
 import com.codesdk.mobile.hashtagapp.util.UiEvent
@@ -22,26 +23,47 @@ class TodoListViewModel @Inject constructor(
     private val _uiEvent = Channel<UiEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
 
+    private var deletedTodo: Todo? = null
+
     fun onEvent(event: TodoListEvent) {
         when (event) {
             is TodoListEvent.OnTodoClick -> {
-
+                // single Todo
+                sendUiEvent(UiEvent.Navigate(Routes.ADD_EDIT_TODO + "?todoId=${event.todo.id}"))
             }
 
             is TodoListEvent.OnAddTodoClick -> {
+                // add Todo
                 sendUiEvent(UiEvent.Navigate(Routes.ADD_EDIT_TODO))
             }
 
             is TodoListEvent.OnUndoDeleteClick -> {
-
+                // undoDelete Todo
+                deletedTodo?.let { todo ->
+                    viewModelScope.launch {
+                        repository.insertTodo(todo)
+                    }
+                }
             }
 
             is TodoListEvent.OnDeleteTodoClick -> {
-
+                // delete Todo
+                viewModelScope.launch {
+                    deletedTodo = event.todo
+                    repository.deleteTodo(event.todo)
+                    sendUiEvent(UiEvent.ShowSnackbar(
+                        "Todo deleted",
+                        "Undo"
+                    ))
+                }
             }
 
             is TodoListEvent.OnDoneChange -> {
-
+                viewModelScope.launch {
+                    event.todo.copy(
+                        isDone = event.isDone
+                    )
+                }
             }
         }
     }
