5e268c333596ea4ea381c00eb9df6e035fe2ccee
Arunabh Das <arunabhdas@gmail.com>
2022-01-27 01:29:40 -0500

    Added TodoListScreen

---
+++ hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/ui/todolist/TodoListScreen.kt
@@ -0,0 +1,73 @@
+package com.codesdk.mobile.hashtagapp.ui.todolist
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarResult
+import androidx.compose.material.Icon
+import kotlinx.coroutines.flow.collect
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.codesdk.mobile.hashtagapp.util.UiEvent
+
+/**
+ * Created by Das on 2022-01-26.
+ */
+@Composable
+fun TodoListScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+    viewModel: TodoListViewModel = hiltViewModel()
+) {
+    val todos = viewModel.todos.collectAsState(initial = emptyList())
+    val scaffoldState = rememberScaffoldState()
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvent.collect { event ->
+            when(event) {
+                is UiEvent.ShowSnackbar -> {
+                    val result = scaffoldState.snackbarHostState.showSnackbar(
+                        message = event.message,
+                        actionLabel = event.action
+                    )
+                    if (result == SnackbarResult.ActionPerformed) {
+                        viewModel.onEvent(TodoListEvent.OnUndoDeleteClick)
+                    }
+                }
+                is UiEvent.Navigate -> onNavigate(event)
+                else -> Unit
+            }
+        }
+    }
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+               viewModel.onEvent(TodoListEvent.OnAddTodoClick)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add"
+                )
+
+            }
+        }
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(todos.value) { todo ->
+                TodoItem(
+                    todo = todo,
+                    onEvent =  viewModel::onEvent
+                )
+            }
+        }
+    }
+}
