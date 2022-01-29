f6c576c9e30760ebc7dad0247757ef7ada729f53
Arunabh Das <arunabhdas@gmail.com>
2022-01-29 17:26:01 -0500

    Getting AddEditTodoViewModel working

--- hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/MainActivity.kt
+++ hashtagapp-android/app/src/main/java/com/codesdk/mobile/hashtagapp/MainActivity.kt
@@ -4,25 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.defaultDecayAnimationSpec
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.codesdk.mobile.hashtagapp.ui.add_edit_todo.AddEditTodoScreen
+import com.codesdk.mobile.hashtagapp.ui.theme.HashtagAppTheme
+import com.codesdk.mobile.hashtagapp.ui.todo_list.TodoListScreen
+import com.codesdk.mobile.hashtagapp.util.Routes
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    fun main() {
-
-        val channelName: String = "CodeSDK"
-        var weight = 160
-        var percentage: Double = 2.5
-        var isAvailable: Boolean = true
-        var s1 = "Call me"
-        var s2 = " Maybe"
-        val names = mutableListOf<String>("Ali", "Maya", "Chen")
-
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.activity_main)
-        main()
+        setContent {
+            HashtagAppTheme() {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.TODO_LIST
+                ) {
+                    composable(Routes.TODO_LIST) {
+                        TodoListScreen(
+                            onNavigate = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                    composable(
+                        route = Routes.ADD_EDIT_TODO + "?todoId={todoId}",
+                        arguments = listOf(
+                            navArgument(name = "todoId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) {
+                        AddEditTodoScreen(onPopBackStack = {
+                            navController.popBackStack()
+                        })
+                    }
+                }
+            }
+        }
     }
 }
